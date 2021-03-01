ea71a5c25a78157084cd4a874e9e5882a711380f
kanekou <e165726@ie.u-ryukyu.ac.jp>
2021-03-01 17:10:51 +0900

    finish ajaxapp

--- ajaxapp/index.js
+++ ajaxapp/index.js
@@ -1,34 +1,48 @@
 console.log("index.js: loaded");
 
-function fetchUserInfo(userId) {
-	fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
-		.then(response => {
-			console.log(response.status);
-			if (!response.ok) {
-				console.error("error response", response);
-			} else {
-				return response.json().then(userInfo => {
-					// HTMLの組み立て
-					// escapeHTMLはタグ付き関数
-					const view = escapeHTML`
-					<h4>${userInfo.name} (@${userInfo.login})</h4>
-					<img src="${userInfo.avatar_url}" alt="$userInfo.login}" height="100">
-					<dl>
-						<dt>Location</dt>
-						<dd>${userInfo.location}</dd>
-						<dt>Repositories</dt>
-						<dd>${userInfo.public_repos}</dd>
-					</dl>
-					`;
+async function main() {
+	try {
+		const userId = getUserId();
+		const userInfo = await fetchUserInfo(userId);
+		const view = createView(userInfo);
+		displayView(view);
+	} catch (error) {
+		console.error(`error response (${error})`);
+	}
+}
 
-					//HTMLの挿入
-					const result = document.getElementById("result");
-					result.innerHTML = view;
-				});
+function getUserId() {
+	return document.getElementById("userId").value;
+}
+
+function fetchUserInfo(userId) {
+	return fetch(`https://api.github.com/users/${encodeURIComponent(userId)}`)
+		.then(response => {
+			if (!response.ok) {
+				return Promise.reject(new Error(`${response.status}: ${response.statusText}`));
+			} else {
+				// JSONオブジェクトで解決されるPromiseを返す
+				return response.json();
 			}
-		}).catch(error => {
-			console.error(error);
-		})
+		});
+}
+
+function createView(userInfo){
+	return escapeHTML`
+	<h4>${userInfo.name} (@${userInfo.login})</h4>
+	<img src="${userInfo.avatar_url}" alt="$userInfo.login}" height="100">
+	<dl>
+		<dt>Location</dt>
+		<dd>${userInfo.location}</dd>
+		<dt>Repositories</dt>
+		<dd>${userInfo.public_repos}</dd>
+	</dl>
+	`;
+}
+
+function displayView(view) {
+	const result = document.getElementById("result");
+	result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
